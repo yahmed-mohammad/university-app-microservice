e4fc20b323896193317538d4246c8872c27b37d6
Yashmin Tabashum <yashminahmed@Yashmins-MacBook-Air.local>
2022-10-25 12:08:31 +0530

    Call service using eureka service name

--- student-service/src/main/java/com/example/feignclient/AddressClient.java
+++ student-service/src/main/java/com/example/feignclient/AddressClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "${address.service.url}", value = "address-client", path = "/api/address")
+@FeignClient(value = "address-service", path = "/api/address")
 public interface AddressClient {
 
     @GetMapping("/{id}")
