23b5965dfdcf5fe6d2490dfbcd5c6679a2b6f98f
Yashmin Tabashum <yashminahmed@Yashmins-MacBook-Air.local>
2022-10-25 12:01:39 +0530

    Added Eureka Server

--- address-service/src/main/java/com/example/AddressApp.java
+++ address-service/src/main/java/com/example/AddressApp.java
@@ -3,11 +3,13 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan("com.example.entity")
 @EnableJpaRepositories("com.example.repository")
+@EnableEurekaClient
 public class AddressApp {
 
     public static void main(String[] args) {
