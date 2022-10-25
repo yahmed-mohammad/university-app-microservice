aea56319531e8285c49e716a33f67f4ff75bcf49
Yashmin Tabashum <yashminahmed@Yashmins-MacBook-Air.local>
2022-10-25 12:52:37 +0530

    Client side load balancing using Spring Cloud Load Balancer

---
+++ student-service/src/main/java/com/example/feignclient/AddressServerLoadBalancerConfig.java
@@ -0,0 +1,16 @@
+package com.example.feignclient;
+
+import feign.Feign;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+
+@LoadBalancerClient(value = "address-service")
+public class AddressServerLoadBalancerConfig {
+
+    @LoadBalanced
+    @Bean
+    public Feign.Builder feignBuilder() {
+        return Feign.builder();
+    }
+}
